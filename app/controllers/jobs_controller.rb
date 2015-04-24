454a9957d4e4a08231e838780698ef4bcd2f1abb
Jess Thomas <jessmariethomas@gmail.com>
2015-04-24 15:34:32 -0700

    Edits jobs with partial form.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,9 +5,14 @@ class JobsController < ApplicationController
 	def new
 		@job=Job.new
 	end
+
 	def create
 		Job.create(job_params)
 		redirect_to jobs_path
+	end
+
+	def edit
+  		@job = Job.find(params[:id])
 	end
 
 	private
